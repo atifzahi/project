19f0c34cf2c423b910ddb08351cad756e4ff6020
Arbaziii <160744848+Arbaziii@users.noreply.github.com>
2025-03-24 19:35:16 +0500

    Update crop.py

--- crop.py
+++ crop.py
@@ -17,7 +17,7 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import warnings
 warnings.filterwarnings('ignore')
-from google.colab import fil
+from google.colab import files
 # -----------------------------
 # Step 1: Load and Inspect Dataset
 # -----------------------------
